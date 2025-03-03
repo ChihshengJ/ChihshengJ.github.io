0cd33238e72910575ef038e06205f8efbd0adace
ChihshengJ <zs_jin@vpn-10-40-15-44.net.rochester.edu>
2025-03-03 02:47:03 -0500

    making html compatible with turkle

--- assets/js/role_script.js
+++ assets/js/role_script.js
@@ -1884,7 +1884,7 @@ var AppView = Backbone.View.extend({
 });
 
 $(document).ready(function () {
-  var dataUrl = getURLParameter("dataUrl");
+  var dataUrl = getURLParameter("dataUrl") || $("#dataUrl").val();
   if (dataUrl) {
     console.log("Loading data from " + dataUrl);
     $.getJSON(dataUrl, function (data) {
